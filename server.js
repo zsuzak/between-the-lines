fec92c0bba1aa965af5be3b45a04edce726d2bee
GBZUDD1 <45389289+GBZUDD1@users.noreply.github.com>
2019-01-24 11:32:53 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {});
+});
+
+app.get('/positions', (req, res) => {
+    res.render('positions.hbs', {});
+});
+
+app.get('/tactics', (req, res) => {
+    res.render('tactics.hbs');
+});
+
+app.get('/formations', (req, res) => {
+    res.render('formations.hbs');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
